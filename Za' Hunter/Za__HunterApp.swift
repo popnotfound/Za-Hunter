d43b5d506e55b4fef1b7934c0c3dedc778d79c68
popnotfound <aneeshleisure21@gmail.com>
2021-07-30 11:21:32 -0500

    Initial Commit

---
+++ Za' Hunter/Za__HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za__HunterApp.swift
+//  Za' Hunter
+//
+//  Created by Aneesh Pushparaj on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Za__HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
