4f169e4d3177c4fec6ce4be33756257007e1505d
popnotfound <aneeshleisure21@gmail.com>
2021-07-30 13:12:17 -0500

    Added pizza icon & Link to restaurant/store website

--- Za' Hunter/ContentView.swift
+++ Za' Hunter/ContentView.swift
@@ -25,7 +25,9 @@ struct ContentView: View {
             showsUserLocation: true,
             userTrackingMode: $userTrackingMode,
             annotationItems: places) {place in
-            MapPin(coordinate: place.annotation.coordinate)
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
         }
         .onAppear(perform: {
             performSearch(item: "Pizza")
@@ -61,4 +63,15 @@ struct Place: Identifiable {
     let id = UUID()
     let annotation: MKPointAnnotation
     let mapItem: MKMapItem
+}
+
+struct Marker:View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("Pizza")
+            })
+        }
+    }
 }
