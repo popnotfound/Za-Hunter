609160041fbe51c15762080d5b42a23bd92f0fd9
popnotfound <aneeshleisure21@gmail.com>
2021-07-30 12:49:36 -0500

    Added user location

---
+++ Za' Hunter/LocationManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocationManager.swift
+//  Za' Hunter
+//
+//  Created by Aneesh Pushparaj on 7/30/21.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
